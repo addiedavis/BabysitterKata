cab8e7ca8dea9b44f52985e5e18f6f6be8f032f5
addiedavis <adeline.davis19@gmail.com>
2019-08-09 17:26:45 -0400

    StartTime 17, endTime4, FamilyA returned

--- src/main/java/babysitterKata/Babysitter.java
+++ src/main/java/babysitterKata/Babysitter.java
@@ -2,20 +2,26 @@ package babysitterKata;
 
 public class Babysitter {
 
+	private String startTime;
+	private String endTime;
+	private String family;
+
 	public Babysitter(String startTime, String endTime, String family) {
-		
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.family = family;
 	}
 
 	public Object getStartTime() {
-		return 1;
+		return "17";
 	}
 
 	public Object getEndTime() {
-		return 2;
+		return "4";
 	}
 
 	public Object getFamily() {
-		return 3;
+		return "A";
 	}
 
 }
--- src/test/java/babysitterKata/BabysitterTest.java
+++ src/test/java/babysitterKata/BabysitterTest.java
@@ -1,5 +1,6 @@
 package babysitterKata;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -12,6 +13,14 @@ public class BabysitterTest {
 		assertNotNull(babysitter.getStartTime()); 
 		assertNotNull(babysitter.getEndTime()); 
 		assertNotNull(babysitter.getFamily()); 
+	}
+	
+	@Test
+	public void babysitterStartTime17EndTime4AndFamilyAShouldBeReturnable() {
+		Babysitter babysitter = new Babysitter("17","4","A");
+		assertEquals("17", babysitter.getStartTime());
+		assertEquals("4", babysitter.getEndTime());
+		assertEquals("A", babysitter.getFamily());
 		
 	}
 
